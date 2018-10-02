2d78b53a5909a340b1df21564557f1f803616079
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-01 20:41:15 -0500

    updating bash.js

--- bash.js
+++ bash.js
@@ -1,11 +1,12 @@
+const commands = require("./commands.js");
+
 // We output a prompt.
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event triggers after a user types in a line.
-process.stdin.on('data', (data) => {
+process.stdin.on('data', (userInput) => {
   // We use trim() to remove the \n character at the end of data.
-  const cmd = data.toString().trim();
+  userInput = userInput.toString().trim();
   
-  process.stdout.write(`You typed: ${cmd}`);
-  process.stdout.write('\nprompt > ');
+  commands.evaluateCmd(userInput);
 });
